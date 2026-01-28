19f64aa812650a5db6b798778f2e38694d3b5f2e
jasmin <jasmin.shanavas@testhouse.net>
2026-01-28 15:08:49 +0530

    Hospital Management System with Docker & CI/CD

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,30 @@
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const patients = [
-  { id: 1, name: "Anu", age: 30, gender: "Female", disease: "Fever" },
-  { id: 2, name: "Rahul", age: 45, gender: "Male", disease: "Diabetes" }
-];
+// Memory storage
+let doctors = [];
+let patients = [];
 
-const doctors = [
-  { name: "Dr. Smith", department: "Cardiology", time: "10AM - 1PM" },
-  { name: "Dr. Meera", department: "Neurology", time: "2PM - 5PM" }
-];
-
-app.get("/patients", (req, res) => {
-  res.json(patients);
+// Routes
+app.post("/addDoctor", (req, res) => {
+    doctors.push(req.body);
+    res.send("Doctor added");
 });
 
 app.get("/doctors", (req, res) => {
-  res.json(doctors);
+    res.json(doctors);
 });
 
-app.listen(8080, () => {
-  console.log("Backend updated");
+app.post("/addPatient", (req, res) => {
+    patients.push(req.body);
+    res.send("Patient added");
 });
+
+app.get("/patients", (req, res) => {
+    res.json(patients);
+});
+
+app.listen(3000, () => console.log("Backend running on http://localhost:3000"));
